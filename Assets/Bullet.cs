0986a586e844d015b5b20d49a01ad8bf2415ae90
VladuxRTK <33584762+VladuxRTK@users.noreply.github.com>
2019-07-29 00:16:25 +0300

    added new unfinished enemy

---
+++ Assets/Bullet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+    private Rigidbody2D rb;
+
+    private Transform player;
+
+
+    private Vector2 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = new Vector2(player.position.x, player.position.y);
+    }
+
+    private void Update()
+    {
+
+        transform.position = Vector2.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+        if (transform.position.x == target.x && transform.position.y == target.y)
+            DestroyProjectile();
+
+    }
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            DestroyProjectile();
+        }
+    }
+}
---
+++ Assets/Enemy2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2 : MonoBehaviour
+{
+    [SerializeField] GameObject bullet;
+
+    public float startTimeBtwShots;
+
+    private float timeBtwShots;
+    private Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBtwShots = startTimeBtwShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBtwShots <= 0 && Vector2.Distance(transform.position, player.transform.position) < 40)
+        {
+            Instantiate(bullet, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+            timeBtwShots -=Time.deltaTime;
+    }
+   
+}
