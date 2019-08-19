d74f1d45ab6a24b7d51c389f3af95fc259d61803
DrBaxR <andiflorea16@yahoo.com>
2019-08-19 16:10:49 +0300

    Added and early version of the level select menu + some functionality for having locked levels

---
+++ Assets/ButtonManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    public int unlockedLevels = 0;
+    public GameObject[] levelButtons;
+
+    private void Start()
+    {
+        int i;
+        for(i = 0; i < unlockedLevels; ++i)
+        {
+            levelButtons[i].GetComponent<Button>().enabled = true;
+            Image lockedMask = levelButtons[i].transform.GetChild(1).GetComponent<Image>();
+            lockedMask.enabled = false;
+        }
+        for(; i < levelButtons.Length; ++i)
+        {
+            levelButtons[i].GetComponent<Button>().enabled = false;
+            Image lockedMask = levelButtons[i].transform.GetChild(1).GetComponent<Image>();
+            lockedMask.enabled = true;
+        }
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
