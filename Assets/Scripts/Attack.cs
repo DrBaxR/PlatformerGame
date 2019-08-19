bca59128a73f44b65f65f250db27a1ddaa727f54
VladuxRTK <33584762+VladuxRTK@users.noreply.github.com>
2019-08-19 21:11:15 +0300

    added laggy sword effect

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class Attack : MonoBehaviour
 {
     public Animator playerAnimator;
+    
+    private AudioSource audioSource;
 
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     private void Update()
     {
         if(Input.GetMouseButtonDown(0) && playerAnimator.GetBool("isJumping"))
         {
             playerAnimator.SetTrigger("attack");
+            audioSource.Play();
         }
     }
 
@@ -18,6 +26,7 @@ public class Attack : MonoBehaviour
     {
         if(collision.tag == "Enemy" && Input.GetMouseButtonDown(0) && playerAnimator.GetBool("isJumping"))
         {
+            
             Damageable enemy = collision.GetComponent<Damageable>();
             enemy.health -= transform.GetComponentInParent<PlayerController>().attackDamage;
         }
