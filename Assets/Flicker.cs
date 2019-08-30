065b7abbe106d7a6cb8d6233c4e451bc3eb289ed
DrBaxR <andiflorea16@yahoo.com>
2019-08-30 18:31:44 +0300

    Added a pause menu

---
+++ Assets/Flicker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//BEWARE: UGLY ASS CODE
+public class Flicker : MonoBehaviour
+{
+    float n;
+    float fps;
+    public GameObject target;
+    bool toDo;
+
+    private void Start()
+    {
+        fps = 1.0f / Time.deltaTime;
+        toDo = false;
+    }
+
+    void Update()
+    {
+        if(n >= fps)
+        {
+            target.SetActive(toDo);
+            toDo = !toDo;
+            n = 0;
+        }
+        else
+        {
+            n++;
+        }
+    }
+}
---
+++ Assets/PauseController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+    public static bool gamePaused;
+    public GameObject pauseUI;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0.0f;
+        pauseUI.SetActive(true);
+        gamePaused = true;
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1.0f;
+        pauseUI.SetActive(false);
+        gamePaused = false;
+    }
+}
