35aedb51fcc58a31620eac43bde767277c1e0e02
DrBaxR <andiflorea16@yahoo.com>
2019-09-03 19:50:15 +0300

    Finished tutorial level
    Added tutorial prompts
    TO DO:
    -fix: prompts and pause menu interaction
    -fix: enemy health flips (?)
    -rethink design of prompts
    -maybe add RPG Maker type text

---
+++ Assets/Scripts/DestroyIfKeyPressed.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyIfKeyPressed : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            Destroy(gameObject);
+            Time.timeScale = 1.0f;
+        }
+    }
+}
---
+++ Assets/Scripts/EnableTarget.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableTarget : MonoBehaviour
+{
+    public GameObject target;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Time.timeScale = 0.0f;
+            target.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
