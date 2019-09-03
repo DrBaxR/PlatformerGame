4050167f07903e4660656acdf56d06ac34ab6938
VladuxRTK <33584762+VladuxRTK@users.noreply.github.com>
2019-09-04 00:48:04 +0300

    added coin pickup sound effect

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public AudioClip coinPickup;    
     AudioSource myAudio;
 
     void Start()
     {
         myAudio =  GetComponent<AudioSource>();
         myAudio.PlayDelayed(3.0f);
+    }
+
+    public void PlaySound(string clip)
+    {
+        if(clip == "coinPickup")
+        {
+            myAudio.PlayOneShot(coinPickup);
+        }
     }
 }
--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     public int collectibleValue;
+    
+    public AudioManager audioPlay;
+
+   
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+       
         if(collision.tag == "Player")
         {
+            // audioClip.clip = pickup;
+            audioPlay.PlaySound("coinPickup");
+            //AudioSource.PlayClipAtPoint(pickup, transform.position);
             GameManager.IncrementScore(collectibleValue);
             Destroy(gameObject);
+            
         }
+       
     }
 }
